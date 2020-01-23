4e85b28768623d37345c10e28850ec6f6c928e50
Olgu <olgusirman@gmail.com>
2020-01-23 16:25:19 +0000

    Initial Commit

---
+++ GithubCombineExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GithubCombineExample
+//
+//  Created by Olgu on 23.01.2020.
+//  Copyright © 2020 Aspendos IT. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
